1c032a5d0a23893d7e9a5799cacdd2262e4ce19f
unknown <lakrainer@edu.aau.at>
2021-06-17 15:30:15 +0200

    classify types of gumtree diffs

--- MavenLauncher_Anwendung/src/main/java/App.java
+++ MavenLauncher_Anwendung/src/main/java/App.java
@@ -11,11 +11,11 @@ import spoon.reflect.CtModelImpl;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.visitor.Filter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        //how decleare the path? how to input other projects?
         String projectOldPath = "D:\\Dokumente\\1_Studium_0-Bachelorarbeit\\MavenLauncher_Aenderungen_feststellen\\Test_Projekte\\Calculator_alt";
         String projectNewPath = "D:\\Dokumente\\1_Studium_0-Bachelorarbeit\\MavenLauncher_Aenderungen_feststellen\\Test_Projekte\\Calculator_neu";
 
@@ -32,9 +32,11 @@ public class App {
         modelOld.getElements(ctElement -> ctElement instanceof CtModelImpl.CtRootPackage).forEach(System.out::println);
         modelNew.getElements(ctElement -> ctElement instanceof CtModelImpl.CtRootPackage).forEach(System.out::println);
 
+        //modelOld.getElements(ctElement -> true).forEach(System.out::println);
+        //modelNew.getElements(ctElement -> true).forEach(System.out::println);
+
         final SpoonGumTreeBuilder scanner = new SpoonGumTreeBuilder();
 
-        //Difference between CtModel and CtElement? --> look Strg+Click Document !!!
         ITree rootSpoonLeft = scanner.getTree(modelOld.getElements(ctElement -> ctElement instanceof CtModelImpl.CtRootPackage).get(0));
         ITree rootSpoonRight = scanner.getTree(modelNew.getElements(ctElement -> ctElement instanceof CtModelImpl.CtRootPackage).get(0));
 
@@ -47,6 +49,25 @@ public class App {
         List<Action> actions = actionGenerator.generate();
 
         actions.forEach(System.out::println);
+
+        ArrayList<Action> inserts = new ArrayList<>();
+        ArrayList<Action> deletes = new ArrayList<>();
+        ArrayList<Action> updates = new ArrayList<>();
+        ArrayList<Action> moves = new ArrayList<>();
+
+        actions.forEach(action -> {
+            if(action.toString().startsWith("INS")){
+                inserts.add(action);
+            }else if(action.toString().startsWith("MOV")){
+                moves.add(action);
+            }else if(action.toString().startsWith("DEL")){
+                deletes.add(action);
+            }else if(action.toString().startsWith("UPD")){
+                updates.add(action);
+            }
+        });
+
+
 
     }
 }
